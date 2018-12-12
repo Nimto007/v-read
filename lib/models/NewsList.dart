26ef7c970eca8060f7786541615c6e25d58290d9
Coder <tonim962@gmail.com>
2018-12-12 16:51:37 +0700

    news app

---
+++ lib/models/NewsList.dart
@@ -0,0 +1,16 @@
+class NewsList {
+  String categoryName;
+  String title;
+  String description;
+  String date;
+  int viewCount;
+  List<String> image;
+
+  NewsList(
+      {this.categoryName,
+      this.title,
+      this.description,
+      this.viewCount,
+      this.date,
+      this.image});
+}
